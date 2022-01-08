f3e9dfb0e2360aeab9d40c4f74f195013b039c4c
yxing6 <yxing6@student.ubc.ca>
2022-01-08 13:42:34 -0800

    A partial working GUI with two individual JCheckBox

--- src/main/ui/gui/ControlPanel.java
+++ src/main/ui/gui/ControlPanel.java
@@ -1,6 +1,5 @@
 package ui.gui;
 
-import model.SelectionState;
 import model.State;
 
 import javax.swing.*;
@@ -13,7 +12,7 @@ public class ControlPanel extends JPanel {
 
     private static final int GAP = 10;  // vertical spacing between components in pixels
     private ISelectionListener selectionListener;
-    private List<StateCheckBox> checkBoxes;
+    private List<JCheckBox> checkBoxes;
 
     // EFFECTS: constructs panel for displaying user controls
     public ControlPanel(ISelectionListener selectionListener) {
@@ -21,7 +20,7 @@ public class ControlPanel extends JPanel {
         this.selectionListener = selectionListener;
         checkBoxes = new ArrayList<>();
         Box toolHolder = Box.createVerticalBox();
-        addRadioButtons(toolHolder);
+        addCheckBox(toolHolder);
         toolHolder.add(Box.createVerticalStrut(GAP));
         add(toolHolder);
     }
@@ -29,27 +28,25 @@ public class ControlPanel extends JPanel {
 
     // MODIFIES: toolHolder
     // EFFECTS: adds radio buttons (visited list and bucket list) to toolHolder
-    private void addRadioButtons(Box toolHolder) {
-        Box radioBtnHolder = Box.createHorizontalBox();
-        radioBtnHolder.setAlignmentX(Box.LEFT_ALIGNMENT);
-        toolHolder.add(radioBtnHolder);
+    private void addCheckBox(Box toolHolder) {
+        Box checkBoxHolder = Box.createHorizontalBox();
+        checkBoxHolder.setAlignmentX(Box.LEFT_ALIGNMENT);
+        toolHolder.add(checkBoxHolder);
 
-        JRadioButton visited = createVisitedButton();
-        JRadioButton bucket = createBucketButton();
+        JCheckBox visited = createVisitedBox();
+        JCheckBox bucket = createBucketBox();
+        checkBoxes.add(visited);
+        checkBoxes.add(bucket);
+        checkBoxHolder.add(visited);
+        checkBoxHolder.add(bucket);
 
-        radioBtnHolder.add(visited);
+        toolHolder.add(checkBoxHolder);
 
-        radioBtnHolder.add(bucket);
-
-        ButtonGroup radioBtnGroup = new ButtonGroup();
-        radioBtnGroup.add(visited);
-        radioBtnHolder.add(Box.createVerticalStrut(GAP));
-        radioBtnGroup.add(bucket);
     }
 
     // EFFECTS: returns radio button for "visited list" option
-    private JRadioButton createVisitedButton() {
-        JRadioButton visited = new JRadioButton("visited list", true);
+    private JCheckBox createVisitedBox() {
+        JCheckBox visited = new JCheckBox("visited list", false);
         visited.addActionListener(new ActionListener() {
             @Override
             public void actionPerformed(ActionEvent e) {
@@ -62,8 +59,8 @@ public class ControlPanel extends JPanel {
 
 
     // EFFECTS: returns radio button for "bucket list" option
-    private JRadioButton createBucketButton() {
-        JRadioButton bucket = new JRadioButton("bucket list", true);
+    private JCheckBox createBucketBox() {
+        JCheckBox bucket = new JCheckBox("bucket list", false);
         bucket.addActionListener(new ActionListener() {
             @Override
             public void actionPerformed(ActionEvent e) {

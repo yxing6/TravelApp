ee0792af69a3c010ed46d7bcafb313d2acfd01d6
yxing6 <yxing6@student.ubc.ca>
2022-01-08 20:58:40 -0800

    name change for StateListener; overload update once again

---
+++ src/main/ui/gui/StateListener.java
@@ -0,0 +1,26 @@
+package ui.gui;
+
+import model.PlaceOfInterest;
+import model.State;
+
+/* CPSC 210 Term Project Version 2:
+ * Travel Record - GUI - ISelectionListener
+ * Author:  Yun Xing
+ * Date:    January 07, 2022,
+ * Apply Observer pattern - method update to be called when changes are observed
+ */
+
+public interface StateListener {
+
+    // called when a new place is added to the program
+    void update(PlaceOfInterest place);
+
+
+    // called when visiting status selection has been changed
+    void update(State state);
+
+
+    // called when user want to display all places
+    void update();
+
+}
